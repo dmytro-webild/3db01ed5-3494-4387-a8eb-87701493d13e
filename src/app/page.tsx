582c6e4bf956d5ead5d2426c1bb2963d7214c841
bender <kudin.dima@gmail.com>
2026-04-10 01:12:48 +0000

    Switch to version 2: modified src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,8 +58,8 @@ export default function HvacPage() {
               { text: "Our Services", href: "#services" },
             ]}
             buttonAnimation="slide-up"
-            videoSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AUhrqIzikJzQFgBimWgvnkX4XL/uploaded-1775783547455-z86hhn0p.jpg"
-            imageAlt="Warm lifestyle scene with woman and dog in premium interior"
+            videoSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AUhrqIzikJzQFgBimWgvnkX4XL/uploaded-1775782472936-kz5t8r5n.jpg"
+            imageAlt="Warm morning scene with woman and dog in cozy interior"
             showDimOverlay={true}
           />
         </div>
